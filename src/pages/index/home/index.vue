f45e4d9add890bd8b5a00f3706e2d48b1bd273f4
wuyue <536465301@qq.com>
2020-05-13 14:07:02 +0800

    提交new_vue模板

---
+++ src/pages/index/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>这是一个空架子</div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+      }
+    },
+    props: {},
+    methods:{},
+    mounted(){
+    },
+    created() {
+    },
+  }
+</script>
+
+<style scoped lang="less">
+</style>
